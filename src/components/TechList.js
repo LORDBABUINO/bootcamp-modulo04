32ad801f02defe088da05252a4538a20e0210462
LORDBABUINO <jorge.x7@gmail.com>
2019-10-02 16:52:27 -0300

    Controls state of a component

--- src/components/TechList.js
+++ src/components/TechList.js
@@ -2,19 +2,39 @@ import React, {Component} from 'react'
 
 class TechList extends Component {
 	state = {
+		newTech: '',
 		techs: [
 			'Node.js',
 			'ReactJS',
 			'React Native',
 		]
 	}
+	handleInputChange = e => {
+		const newTech = e.target.value
+		this.setState({newTech})
+	}
+	handleSubmit = e => {
+		e.preventDefault()
+		const {techs, newTech} = this.state
+		this.setState({
+			techs: [...techs, newTech],
+			newTech: ''
+		})
+	}
+	
 	render() {
 		return (
+			<form onSubmit={this.handleSubmit}>
 			<ul>
-				<li>Node.js</li>
-				<li>React.js</li>
-				<li>React Native</li>
+				{this.state.techs.map((tech, index) => <li key={index}>{tech}</li>)}
 			</ul>
+			<input
+				type="text"
+				value={this.state.newTech}
+				onChange={this.handleInputChange}
+			/>
+					<button type="submit">Enviar</button>
+			</form>
 		)
 	}
 }
