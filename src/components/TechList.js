1fe3c21b7c0ab7d7c1599fa44630524bd2095d4e
LORDBABUINO <jorge.x7@gmail.com>
2019-10-03 15:02:06 -0300

    Adds life cicle functions

--- src/components/TechList.js
+++ src/components/TechList.js
@@ -5,12 +5,25 @@ import TechItem from './TechItem'
 class TechList extends Component {
 	state = {
 		newTech: '',
-		techs: [
-			'Node.js',
-			'ReactJS',
-			'React Native',
-		]
+		techs: []
 	}
+	//Executado assim que o componente aparece em tela
+	componentDidMount() {
+		const techs = localStorage.getItem('techs')
+		techs && this.setState({techs: JSON.parse(techs)})
+	}
+
+	//Executado sempre que houver alterações nas props ou estado
+	componentDidUpdate(_, prevState) {
+		const {techs} = this.state
+		prevState.techs !== techs && localStorage.setItem('techs', JSON.stringify(techs))
+	}
+	
+	//Executado quando o componente deixa de existir
+	componentWillUnmount() {
+
+	}
+	
 	handleInputChange = e => {
 		const newTech = e.target.value
 		this.setState({newTech})
